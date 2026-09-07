e5f87e93684d6d02146dd0534e15394b829a0ae3
Rocio Ricciardiello <paragit@gmail.com>
2026-09-07 10:31:42 +0200

    Add Curso class

--- src/main/java/konectia/cryptonectia/modelo/entidad/Curso.java
+++ src/main/java/konectia/cryptonectia/modelo/entidad/Curso.java
@@ -1,53 +1,39 @@
 package konectia.cryptonectia.modelo.entidad;
-
 public class Curso {
-
     private int id;
     private String nombre;
     private int horas;
     private boolean activo;
-
     public Curso(int id, String nombre, int horas, boolean activo) {
         this.id = id;
         this.nombre = nombre;
         this.horas = horas;
         this.activo = activo;
     }
-
     public Curso(String nombre, int horas, boolean activo) {
         this.nombre = nombre;
         this.horas = horas;
         this.activo = activo;
     }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public int getHoras() {
         return horas;
     }
-
     public void setHoras(int horas) {
         this.horas = horas;
     }
-
     public boolean isActivo() {
         return activo;
     }
-
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
